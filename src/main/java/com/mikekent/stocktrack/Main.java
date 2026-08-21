411b5f6a2a3007520c7ea2b18392683d7988c173
mikekent72 <mikekent727@gmail.com>
2026-08-21 12:24:50 +0100

    Initialise StockTrack project

---
+++ src/main/java/com/mikekent/stocktrack/Main.java
@@ -0,0 +1,25 @@
+package com.mikekent.stocktrack;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("StockTrack");
+
+        Scene scene = new Scene(new StackPane(label), 800, 500);
+
+        stage.setTitle("StockTrack");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/test/java/com/mikekent/stocktrack/MainTest.java
@@ -0,0 +1,13 @@
+package com.mikekent.stocktrack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MainTest {
+
+    @Test
+    void applicationTest() {
+        assertTrue(true);
+    }
+}
